8b20ba16885e45edf181806be34df0cf8399e86e
Gabriel Viana <gabrielviana@tivic.com.br>
2023-05-16 10:44:35 -0300

    innitial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <QuillEditor content-type="html" v-model:content="teste" />
+  <div v-html="teste"></div>
+</template>
+
+<script setup lang="ts">
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
+import { ref } from "vue";
+
+const teste = ref("");
+</script>
+
+<style scoped></style>
